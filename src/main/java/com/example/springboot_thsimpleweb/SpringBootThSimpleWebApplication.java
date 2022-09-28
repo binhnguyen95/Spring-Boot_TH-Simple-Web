663fab232bafca0d8ac7b8a953ac2e936451f053
binhnguyen95 <invalidmyarse@gmail.com>
2022-09-28 09:35:20 +0700

    done

---
+++ src/main/java/com/example/springboot_thsimpleweb/SpringBootThSimpleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_thsimpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThSimpleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThSimpleWebApplication.class, args);
+    }
+
+}
